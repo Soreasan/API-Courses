9ae9f846bebf29fc9ef8ea69112401f1053ad048
Kenneth Adair <SOREASAN@GMAIL.COM>
2015-10-15 08:40:27 -0600

    Josh Jensen fixed database connection
    Added a single line of code to ensure the database was connecting.

--- v1/src/Utilities/DatabaseConnection.php
+++ v1/src/Utilities/DatabaseConnection.php
@@ -31,6 +31,7 @@ class DatabaseConnection
                 $connectionString = "mysql:host=".static::$host.";dbname=".static::$dbname;
                 static::$instance = new \PDO($connectionString, static::$user, static::$pass);
                 static::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+		return static::$instance;
             } catch (PDOException $e) {
                 echo "Unable to connect to the database: " . $e->getMessage();
                 die();
