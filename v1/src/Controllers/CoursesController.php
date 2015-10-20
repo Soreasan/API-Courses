e6e2872cc07880ca90447ddb53bb9514ed1af272
andporter <andrew@andrewlaptop.wireless.weber.edu>
2015-10-20 10:20:43 -0600

    clean up post function - separate some logic into their own functions

--- v1/src/Controllers/CoursesController.php
+++ v1/src/Controllers/CoursesController.php
@@ -73,35 +73,21 @@ class CoursesController
         $json_input = (object) json_decode(file_get_contents('php://input')); //Decode raw payload / json
         $input = Cast::cast("\\TestingCenter\\Models\\Course", $json_input); //Cast to Course Data Object
 
-        if (is_null($input)) {
-            http_response_code(Http\StatusCodes::BAD_REQUEST);
-            exit("No data to post.");
-        }
+        $this->validatePOSTInput($input);
 
         $pdo = DatabaseConnection::getInstance();
 
         //Check if CourseData already exists (courseNumber or courseTitle)
-        $sql = $pdo->prepare("SELECT courseData_id FROM CourseData WHERE courseNumber = :courseNumber OR courseTitle = :courseTitle");
-        $data = array("courseNumber" => $input->getCourseNumber(), "courseTitle" => $input->getCourseTitle());
-        $sql->execute($data);
-        $sqlResults = $sql->fetchAll();
+        $courseData_id = $this->getCourseData_Id($pdo, $input);
 
-        //get $courseData_id -- create new CourseData if needed
-        if (empty($sqlResults)) {
-            // CourseData doesnt exist. Make one.
-            $sql = $pdo->prepare("INSERT INTO CourseData (courseNumber, courseTitle) VALUES (:courseNumber, :courseTitle) ");
-            $data = array("courseNumber" => $input->getCourseNumber(), "courseTitle" => $input->getCourseTitle());
+        try {
+            //Create new Courses
+            $sql = $pdo->prepare("INSERT INTO Courses (courseCRN, courseYear, courseSemester, courseData_id) VALUES (:courseCRN, :courseYear, :courseSemester, :courseData_id)");
+            $data = array("courseCRN" => $input->getCourseCRN(), "courseYear" => $input->getCourseYear(), "courseSemester" => $input->getCourseSemester(), "courseData_id" => $courseData_id);
             $sql->execute($data);
-
-            $courseData_id = $pdo->lastInsertId();
-        } else {
-            $courseData_id = (int) $sqlResults[0]["courseData_id"];
+        } catch (PDOException $e) {
+            echo "Error: " . $e;
         }
-
-        //Create new Courses
-        $sql = $pdo->prepare("INSERT INTO Courses (courseCRN, courseYear, courseSemester, courseData_id) VALUES (:courseCRN, :courseYear, :courseSemester, :courseData_id)");
-        $data = array("courseCRN" => $input->getCourseCRN(), "courseYear" => $input->getCourseYear(), "courseSemester" => $input->getCourseSemester(), "courseData_id" => $courseData_id);
-        $sql->execute($data);
     }
 
     //delete, deletes stuff....
@@ -129,5 +115,45 @@ class CoursesController
     public function options()
     {
         header("Allow: " . implode(", ", $this->options));
+    }
+
+    private function validatePOSTInput($input)
+    {
+        if (empty($input->getCourseCRN())) {
+            http_response_code(Http\StatusCodes::BAD_REQUEST);
+            exit("CourseCRN Required");
+        }
+        if (empty($input->getCourseNumber())) {
+            http_response_code(Http\StatusCodes::BAD_REQUEST);
+            exit("Course Number Required");
+        }
+        if (empty($input->getCourseYear())) {
+            http_response_code(Http\StatusCodes::BAD_REQUEST);
+            exit("Course Year Required");
+        }
+        if (empty($input->getCourseSemester())) {
+            http_response_code(Http\StatusCodes::BAD_REQUEST);
+            exit("Course Semester Required");
+        }
+    }
+
+    private function getCourseData_Id($pdo, $input)
+    {
+        $sql = $pdo->prepare("SELECT courseData_id FROM CourseData WHERE courseNumber = :courseNumber OR courseTitle = :courseTitle");
+        $data = array("courseNumber" => $input->getCourseNumber(), "courseTitle" => $input->getCourseTitle());
+        $sql->execute($data);
+        $sqlResults = $sql->fetchAll();
+
+        //get $courseData_id -- create new CourseData if needed
+        if (empty($sqlResults)) {
+            // CourseData doesnt exist. Make one.
+            $sql = $pdo->prepare("INSERT INTO CourseData (courseNumber, courseTitle) VALUES (:courseNumber, :courseTitle) ");
+            $data = array("courseNumber" => $input->getCourseNumber(), "courseTitle" => $input->getCourseTitle());
+            $sql->execute($data);
+
+            return $pdo->lastInsertId();
+        } else {
+            return (int) $sqlResults[0]["courseData_id"];
+        }
     }
 }
